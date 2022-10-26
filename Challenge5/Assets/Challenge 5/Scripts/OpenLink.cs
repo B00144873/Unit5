d6edfd6340f5a2eca12c80af7e0cd6bc4eec06f1
B00144873 <B00144873@mytudublin.ie>
2022-10-26 20:42:13 +0100

    Challenge5

---
+++ Challenge5/Assets/Challenge 5/Scripts/OpenLink.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenLink : MonoBehaviour
+{
+    void Start()
+    {  
+    }
+
+    void Update()
+    {
+    }
+
+    public void OpenChannel()
+    {
+        Application.OpenURL("https://github.com/B00144873/Unit5");
+    }
+}
